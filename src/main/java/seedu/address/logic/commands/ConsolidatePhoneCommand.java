84dc07e2ef92df739161df057b5a3ee1d53268ab
3seastacks <194687609+3seastacks@users.noreply.github.com>
2025-10-16 23:10:32 +0800

    Add Javadoc comment

--- src/main/java/seedu/address/logic/commands/ConsolidatePhoneCommand.java
+++ src/main/java/seedu/address/logic/commands/ConsolidatePhoneCommand.java
@@ -1,14 +1,18 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import static java.util.Objects.requireNonNull;
 
+/**
+ * Consolidate all the student's phone number.
+ */
 public class ConsolidatePhoneCommand extends Command {
 
     public static final String COMMAND_WORD = "consolidate:phone";
