f20abba48ed7eb0e82ef5cb3588215b782d94bbd
3seastacks <194687609+3seastacks@users.noreply.github.com>
2025-10-20 22:11:41 +0800

    Update import

--- src/test/java/seedu/address/logic/commands/ConsolidatePhoneCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ConsolidatePhoneCommandTest.java
@@ -1,21 +1,15 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.TypicalPersons;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.*;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.DANIEL;
 
 public class ConsolidatePhoneCommandTest {
 
